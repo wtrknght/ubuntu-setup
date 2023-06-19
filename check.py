e4bc46f680bf91a4ab08d77df84a4b497b7a7833
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-06-19 15:08:54 -0400

    Create check.py

---
+++ check.py
@@ -0,0 +1,9 @@
+import os
+hostname = "192.168.10.124" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print(f"{hostname} is up!")
+else:
+  print(f"{hostname} is down!")
