9d63e173fe68b24c898ac4d16f773b735649b20b
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-06-19 15:10:52 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,7 +1,7 @@
 import os
 from time import sleep
 hostname = "192.168.10.124" #example
-response = os.system("ping -c 1 " + hostname)
+response = os.system('ping %s -n 1' % (hostname,))
 
 #and then check the response...
 if response == 0:
