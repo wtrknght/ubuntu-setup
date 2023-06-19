951724bd65a5075b36790bbc16877ad6d05b4ec7
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-06-19 15:21:21 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,11 +1,9 @@
 import os
-from time import sleep
 hostname = "192.168.10.124" #example
-response = os.system('ping %s -n 1' % (hostname,))
+response = os.system("ping -n 1 " + hostname)
 
 #and then check the response...
 if response == 0:
   print(f"{hostname} is up!")
 else:
   print(f"{hostname} is down!")
-sleep(5)
